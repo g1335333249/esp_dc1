e27d9f106357a343302853981ae7939cbefd719c
g1335333249 <g1335333249@gmail.com>
2026-05-21 08:57:23 +0800

    fix: 改回 CPPDEFINES 定义宏，修复版本号仍显示 dev
    BUILD_FLAGS 不是 SCons 标准变量，追加不生效。
    改用 CPPDEFINES + '"xxx"' 引号形式，确保 C 预处理器正确识别为字符串字面量。

--- scripts/generate-build-info.py
+++ scripts/generate-build-info.py
@@ -6,10 +6,10 @@ now = datetime.datetime.utcnow() + datetime.timedelta(hours=8)
 version = now.strftime("%Y%m%d")
 date_str = now.strftime("%Y-%m-%d %H:%M:%S")
 
-# 用 BUILD_FLAGS 直接传递 -D 参数，避免 SCons CPPDEFINES 引号转义问题
-env.Append(BUILD_FLAGS=[
-    '-DBUILD_VERSION=\\"{}\\"'.format(version),
-    '-DBUILD_DATE=\\"{}\\"'.format(date_str)
+# 通过 CPPDEFINES 定义宏，value 用 '"xxx"' 形式确保 C 预处理器将其识别为字符串字面量
+env.Append(CPPDEFINES=[
+    ("BUILD_VERSION", '"{}"'.format(version)),
+    ("BUILD_DATE", '"{}"'.format(date_str))
 ])
 
 print("BuildInfo: version={}, date={}".format(version, date_str))
