3e8b0f8d4ed6957140e5cdb485fe5e82171d0fc7
g1335333249 <g1335333249@gmail.com>
2026-05-21 08:50:36 +0800

    fix: 改用 BUILD_FLAGS 传递版本号宏，修复版本号显示 dev 和时间差 8 小时
    - CPPDEFINES 引号转义在 SCons 中不可靠，改为 BUILD_FLAGS 直接传 -D 参数
    - 时区计算改为 utcnow() + timedelta(hours=8)，更清晰不受本地时区影响
    - 清理废弃的 include/BuildInfo.h

--- scripts/generate-build-info.py
+++ scripts/generate-build-info.py
@@ -2,14 +2,14 @@ Import('env')
 import datetime
 
 # 固定使用北京时间 UTC+8（中国不实行夏令时）
-now = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=8)))
-version = now.strftime("%Y%m%d")
+now = datetime.datetime.utcnow() + datetime.timedelta(hours=8)
+version = now.strftime("%y%m%d")
 date_str = now.strftime("%Y-%m-%d %H:%M:%S")
 
-# 通过 build flags 直接定义宏，所有源文件无需 include 头文件即可使用
-env.Append(CPPDEFINES=[
-    ("BUILD_VERSION", '\\"{}\\"'.format(version)),
-    ("BUILD_DATE", '\\"{}\\"'.format(date_str))
+# 用 BUILD_FLAGS 直接传递 -D 参数，避免 SCons CPPDEFINES 引号转义问题
+env.Append(BUILD_FLAGS=[
+    '-DBUILD_VERSION=\\"{}\\"'.format(version),
+    '-DBUILD_DATE=\\"{}\\"'.format(date_str)
 ])
 
 print("BuildInfo: version={}, date={}".format(version, date_str))
